dec69853f0b7713464593b0afbbbda9fe03debee
Elzenar Olorea <elzenar@mail.ru>
2011-12-16 13:00:08 +0200

    Created a first version of Rectangle class

--- lib/Rectangle.php
+++ lib/Rectangle.php
@@ -8,11 +8,29 @@ class NegativeValueException extends Exception{}
 
 class Rectangle{
 
+    protected $length;
+    protected $width;
+
+    public function __construct($length, $width)
+    {
+        if ( ($length < 0) || ($width < 0)) {
+            throw new NegativeValueException ('Negative values are forbidden');
+        }
+        if ( (is_null($length)) || (is_null($width))) {
+            throw new NullValueException ('Values should not be empty');
+        }
+        $this->length = $length;
+        $this->width = $width;
+
+    }
+
     public function calculateSquare(){
+        return $this->width * $this->length;
 
     }
 
     public function calculatePerimeter(){
+        return 2 * ($this->width + $this->length);
 
     }
 
