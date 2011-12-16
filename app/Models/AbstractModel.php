1212ef142e0b69257bc5b6a0f65fe7c9b6281fbf
Ivan Mosiev <ivan.mosiev@altexsoft.com>
2011-12-16 13:03:01 +0200

    Added models

---
+++ app/Models/AbstractModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace Models;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: verber
+ * Date: 16.12.11
+ * Time: 13:02
+ * To change this template use File | Settings | File Templates.
+ */
+class AbstractModel
+{
+
+}
